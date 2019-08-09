6ffe777ddac4d91b59db6b922118493abb3df7b3
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 15:29:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
   # return an empty hash
+  return Hash.new 
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  return {"haha" => "hehe"}
 end
 
 def pioneer
